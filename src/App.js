dd4c1d1d620765baff62a520510a8b5afc93cb3a
Keivin <keivinbr13@gmail.com>
2020-05-18 18:15:04 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import posts from './posts'
 
 
 // Modifica el componente App para implmentar la funcionalidad requerida
-
 class App extends Component {
+
+  state={
+    filt: '',
+  }
+
+  filtro = (e) =>{
+    this.setState({
+      filt: e.target.value,
+    })
+  }
+  
+
+
   render() {
+    let filterPost = posts.filter((post) => {
+      return post.title.toLowerCase().indexOf(this.state.filt.toLowerCase()) !== -1;
+    });
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input 
+            type="text" 
+            placeholder="Ingresa el término de búsqueda" 
+            onChange={this.filtro}
+          />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
+          {filterPost.map((e, ind) =>
+            <li key={ind}>
+            <a href={e.url}><img src={e.image } /></a>
+            <p>{ e.title }</p>
           </li>
+          )}
+          
         </ul>
       </div>
     )
